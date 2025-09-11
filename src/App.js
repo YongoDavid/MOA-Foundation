8cebccb2a831ee835d7452cb38da3af1d57ae9a4
Yongo David <davidyongo9@gmail.com>
2025-09-11 16:44:20 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import ProgramsSection from "./components/ProgramsSection"
 import AboutSection from "./components/AboutSection"
-import TestimonialsCarousel from "./components/TestimonialsCarousel"
+// import TestimonialsCarousel from "./components/TestimonialsCarousel"
 import NewsletterSection from "./components/NewsletterSection"
 import Footer from "./components/Footer"
 
